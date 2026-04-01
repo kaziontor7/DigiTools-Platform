ab804e5f4262bf4387ce4677563f3af69b9e14c0
kaziontor7 <kaziontor7@gmail.com>
2026-04-02 02:39:02 +0600

    Products card

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -2,8 +2,36 @@ import React from 'react';
 
 const Products = ({tool}) => {
     return (
-        <div>
-            
+        <div className=''>
+            <div className="card  bg-base-100 shadow-sm h-full">
+  <div className="card-body">
+    <div className='flex justify-between'>
+        <div className='rounded-[50%] border border-gray-200 w-15 h-15'>
+           <img className=' w-8 h-8 mx-auto my-3' src={tool.icon} alt="" />
+        </div>
+           <span className={`px-3 py-2.5 rounded-full font-medium text-sm ${tool.tag==='Popular'? 'badge badge-xs badge-warning' : ''}
+           ${tool.tag==='New'? 'badge badge-xs badge-success' : ''} 
+           ${tool.tag==='Best Seller'? 'badge badge-xs badge-info' : ''}`}>{tool.tag}</span>  
+      
+    </div>
+    
+    <div className="space-y-4">
+      <h2 className="text-3xl font-bold">{tool.name}</h2>
+      <p className='gray-color'>{tool.description}</p>
+      <span className="text-base gray-color"><span className='textColor font-bold text-2xl'>${tool.price}</span>/{tool.period}</span>
+    </div>
+    <ul className="mt-4 flex flex-col gap-2 text-base font-medium  gray-color grow">
+      {tool.features.map(feature=><li>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+        <span className=''>{feature}</span>
+      </li>)}
+      
+    </ul>
+    <div className="mt-4">
+      <button className="btn gradient btn-block font-bold text-white text-base rounded-full">Buy Now</button>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -9,7 +9,7 @@ const Shop = ({toolsPromise}) => {
      <div>
         <div className='space-y-2 md:space-y-4 md:mt-28 mt-14 md:mb-10 mb-5'>
             <h1 className='textColor md:text-5xl text-3xl font-extrabold text-center'>Premium Digital Tools</h1>
-            <p className='gray-color text-center'>Choose from our curated collection of premium digital products designed to boost your productivity and creativity. </p>
+            <p className='gray-color text-center'>Choose from our curated collection of premium digital products designed <br /> to boost your productivity and creativity. </p>
             <div className='bg-white rounded-full p-0.5 drop-shadow-lg w-fit my-5 flex items-center mx-auto'>
                 <button onClick={()=>setBtnState('products')} className={`${btnState === 'products' ? 'btn gradient py-3.5 px-6 font-bold text-white rounded-full' : 'btn bg-white border-none w-fit rounded-full shadow-none py-0.5 px-6 font-medium text-black' }`}>Products</button>
                 <button onClick={()=>setBtnState('cart')} className={`${btnState === 'cart' ? 'btn gradient py-3.5 px-6 font-bold text-white rounded-full' : 'btn bg-white border-none w-fit rounded-full shadow-none py-0.5 px-6 font-medium text-black' }`}>Cart(2)</button>
@@ -17,7 +17,7 @@ const Shop = ({toolsPromise}) => {
         </div>
         <div>
             {
-                btnState === 'products' ? tools.map(tool=><Products  tool={tool}></Products>) : <Cart></Cart>
+                btnState === 'products' ?<div className='grid md:grid-cols-2 lg:grid-cols-3 md:w-8/10 w-9/10 gap-6 mx-auto'>{tools.map(tool=><Products  tool={tool}></Products>)} </div> : <Cart></Cart>
             }
         </div>
 
