fdceff59373169d9499d34f4ec74ee52d3a624e6
kaziontor7 <kaziontor7@gmail.com>
2026-04-01 23:38:32 +0600

    added btn functionality

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,18 +1,26 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
+import Products from '../Products/Products';
+import Cart from '../Cart/Cart';
 
 const Shop = ({toolsPromise}) => {
     const tools = use(toolsPromise);
-    console.log(tools)
+    const [btnState,setBtnState]= useState('products')
     return (
      <div>
-        <div className='space-y-2 md:space-y-4'>
+        <div className='space-y-2 md:space-y-4 md:mt-28 mt-14 md:mb-10 mb-5'>
             <h1 className='textColor md:text-5xl text-3xl font-extrabold text-center'>Premium Digital Tools</h1>
             <p className='gray-color text-center'>Choose from our curated collection of premium digital products designed to boost your productivity and creativity. </p>
             <div className='bg-white rounded-full p-0.5 drop-shadow-lg w-fit my-5 flex items-center mx-auto'>
-                <button className='btn gradient py-3.5 px-6 font-bold text-white rounded-full'>Products</button>
-                <button className='  py-0.5 px-6 font-medium text-black '>Cart(2)</button>
+                <button onClick={()=>setBtnState('products')} className={`${btnState === 'products' ? 'btn gradient py-3.5 px-6 font-bold text-white rounded-full' : 'btn bg-white border-none w-fit rounded-full shadow-none py-0.5 px-6 font-medium text-black' }`}>Products</button>
+                <button onClick={()=>setBtnState('cart')} className={`${btnState === 'cart' ? 'btn gradient py-3.5 px-6 font-bold text-white rounded-full' : 'btn bg-white border-none w-fit rounded-full shadow-none py-0.5 px-6 font-medium text-black' }`}>Cart(2)</button>
             </div>
         </div>
+        <div>
+            {
+                btnState === 'products' ? tools.map(tool=><Products  tool={tool}></Products>) : <Cart></Cart>
+            }
+        </div>
+
     </div>
     );
 };
